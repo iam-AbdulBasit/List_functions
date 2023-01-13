af5a8b0a0dab6604ea18d3aac49d0fe4c7f54e08
iam-AbdulBasit <120900763+iam-AbdulBasit@users.noreply.github.com>
2023-01-14 02:16:24 +0500

    Create List_function.py

---
+++ List_function.py
@@ -0,0 +1,27 @@
+numbers =['1', '2', '3', '12', '9', '6', '5']
+#
+numbers.sort()
+#
+numbers.reverse()
+#
+numbers.extend([10])
+#
+numbers.append(12)
+#
+numbers.insert(1, 35)
+#
+numbers.remove(7)
+#
+numbers.pop()
+#
+numbers.pop()
+#
+print(numbers)
+#
+print(len(numbers))
+print(max(numbers))
+#
+print(min(numbers))
+#
+numbers[1]=23
+print(numbers)
